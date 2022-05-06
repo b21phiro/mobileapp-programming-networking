bdfed4d423f8bd4db05e6be5e4f1da45ea00c1b3
PhilipRosenqvist <philip.rosenqvist@outlook.com>
2022-05-06 10:38:43 +0200

    Implemented Gson

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -6,8 +6,17 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private ArrayList<Mountain> mountains;
 
     private RecyclerView recyclerView;
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
@@ -17,15 +26,15 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         recyclerView = findViewById(R.id.recyclerView);
-
         new JsonFile(this, this).execute(JSON_URL);
     }
 
     @Override
     public void onPostExecute(String json) {
-        Log.d("MainActivity", json);
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<Mountain>>(){}.getType();
+        mountains = gson.fromJson(json, type);
     }
 
 }
